e5352e6daf5a4eaf8060e824d9d51dee23023697
Cristian Berneanu <cristian@aircloak.com>
2016-10-07 11:34:33 +0300

    Delete old deploy before each new deploy

--- run.sh
+++ run.sh
@@ -4,9 +4,12 @@ cd "$(dirname "$0")"
 
 # make and publish nightly build
 echo "Deploying nightly build ..."
+if [ -f ./logs/deploy.log ]; then
+  mv ./logs/deploy.log ./logs/deploy.old.log
+fi
 if ! (set -o pipefail && cd aircloak && git pull && ./publish.sh nightly 2>&1 | tee "../logs/deploy.log") ; then
   mail everyone-dev@aircloak.com -aFrom:everyone-dev@aircloak.com -s "Nightly deploy failed :(" < "./logs/deploy.log"
-  echo "Nightly build failed! Notification email sent."  
+  echo "Nightly build failed! Notification email sent."
   exit
 fi
 
