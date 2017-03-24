7627cac7f342de66bd11a9c5db7a80b623e5c0b8
Paweł Obrok <pawel.obrok@gmail.com>
2017-03-24 16:33:27 +0100

    Add failed login test

--- frontend/test/login_test.js
+++ frontend/test/login_test.js
@@ -1,5 +1,15 @@
 import assert from "assert";
 
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 describe("login", () => {
-  it("works", () => assert.equal(2 + 2, 4));
+  it("shows a message for incorrect login info", () => {
+    browser.url("/auth");
+    browser.setValue("[name='email']", "no.such@person.org");
+    browser.setValue("[name='password']", "1234");
+    browser.click("form button");
+
+    browser.waitForText(".alert-danger", "Invalid e-mail or password.");
+    assert(browser.getUrl().endsWith("/auth"));
+  });
 });
