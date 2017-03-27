d7feea19ce674c6dd6dedceb40a346e8431dec72
Paweł Obrok <pawel.obrok@gmail.com>
2017-03-27 12:16:44 +0200

    Add a task to start the selenium container

---
+++ frontend/start_dependencies.sh
@@ -0,0 +1,14 @@
+CONTAINER_NAME=selenium
+IMAGE_NAME=selenium/firefox-standalone
+
+function named_container_running {
+  if [ -z "$(docker ps --filter=name=$1 | grep -w $1)" ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+if ! named_container_running $CONTAINER_NAME ; then
+	docker run --name $CONTAINER_NAME -d -p 4444:4444 selenium/standalone-firefox:latest
+fi
