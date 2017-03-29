1183eaeed2282d5bc942f577d49806b1c5fd0cdc
Paweł Obrok <pawel.obrok@gmail.com>
2017-03-29 16:26:02 +0200

    Add a successful login test

--- frontend/test/login_test.js
+++ frontend/test/login_test.js
@@ -1,5 +1,7 @@
 import assert from "assert";
 
+import {admin} from "../config.json";
+
 describe("login", () => {
   it("shows a message for incorrect login info", () => {
     browser.url("/auth");
@@ -9,5 +11,14 @@ describe("login", () => {
 
     browser.waitForText(".alert-danger", "Invalid e-mail or password.");
     assert(browser.getUrl().endsWith("/auth"));
+  });
+
+  it("allows login for correct login info", () => {
+    browser.url("/auth");
+    browser.setValue("[name='email']", admin.email);
+    browser.setValue("[name='password']", admin.password);
+    browser.click("form button");
+
+    browser.waitForText(".alert-info", "Logged in successfully.");
   });
 });
