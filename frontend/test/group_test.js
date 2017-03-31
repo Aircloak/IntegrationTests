2a87475b63fb611f1cc3653d25c0e51295bb99e2
Paweł Obrok <pawel.obrok@gmail.com>
2017-03-31 16:52:23 +0200

    Add test for removing a group

--- frontend/test/group_test.js
+++ frontend/test/group_test.js
@@ -2,6 +2,7 @@ import assert from "assert";
 
 import {loginAdmin} from "./support/session";
 import {randomString} from "./support/random";
+import {createGroup} from "./support/group";
 
 describe("managing groups", () => {
   before(loginAdmin);
@@ -17,5 +18,16 @@ describe("managing groups", () => {
 		assert(browser.isExisting(`h2*=Edit ${name}`));
 		browser.url("/admin/groups");
 		assert(browser.isExisting(`tr*=${name}`));
+  });
+
+  it("allows removing a group", () => {
+    const {name} = createGroup();
+
+    browser.url("/admin/groups");
+    browser.element(`tr*=${name}`).click("a*=Delete");
+    browser.waitUntil(() => browser.getSource().includes("Group deleted"));
+
+    browser.url("/admin/groups");
+    assert.equal(browser.getSource().includes(name), false);
   });
 });
---
+++ frontend/test/support/group.js
@@ -0,0 +1,12 @@
+import {randomString} from "./random";
+
+export const createGroup = () => {
+  const name = randomString();
+
+  browser.url("/admin/groups");
+  browser.setValue("#group_name", name);
+  browser.click("input[value='Add group']");
+  browser.waitUntil(() => browser.getSource().includes("Group created"));
+
+  return {name};
+}
