c9fe7abdc4d62432114aa8c58cb25f876c8b0d57
Sebastian Probst Eide <sebastian@aircloak.com>
2017-08-17 14:56:23 +0200

    Deploy shared common files

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,6 @@ scp run.sh srv-76-131:/aircloak/tests/integration/
 ./backend/deploy.sh # backend files
 ./frontend/deploy.sh # frontend files
 ./compliance/deploy.sh # compliance files
+
+# Deploy common files
+scp -r common srv-76-131:/aircloak/tests/integration/
