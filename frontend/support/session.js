c94d50e61f6809ce2cfc49c79654088d1bb23d0c
Paweł Obrok <pawel.obrok@gmail.com>
2018-05-02 13:26:38 +0200

    Update for new text on privacy policy button

--- frontend/support/session.js
+++ frontend/support/session.js
@@ -14,12 +14,16 @@ export const login = (user) => {
   browser.setValue("[name='password']", user.password);
   browser.click("form button");
 
-  browser.waitUntil(() =>
-    browser.getSource().includes("Sign out") || browser.getSource().includes("Accept privacy policy")
-  );
+  browser.waitUntil(isLoggedIn() || isOnPrivacyPolicyPage());
 
-  if (browser.getSource().includes("Accept privacy policy")) {
+  if (isOnPrivacyPolicyPage()) {
     browser.click("#review-box form button");
-    browser.waitUntil(() => browser.getSource().includes("Sign out"));
+    browser.waitUntil(isLoggedIn);
   }
 }
+
+const isOnPrivacyPolicyPage = () =>
+  browser.getSource().includes("Accept privacy policy") || browser.getSource().includes("Consent to privacy policy");
+
+const isLoggedIn = () =>
+  browser.getSource().includes("Sign out");
